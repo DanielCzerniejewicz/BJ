3ca9fcad639016a32fc6947549d825966e8714b2
Daniel <programowaniedaniel@gmail.com>
2024-05-19 00:17:03 +0200

    Initial commit

---
+++ Black Jack/Program.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Black_Jack
+{
+    class Karta
+    {
+        public string Wartosc { get; set; }
+        public string Kolor { get; set; }
+
+        public Karta()
+        {
+            Wartosc = "Nie Ustawiono!";
+            Kolor = "Nie Ustawiono!";
+        }
+
+        public Karta(string wartosc, string kolor)
+        {
+            Wartosc = wartosc;
+            Kolor = kolor;
+        }
+    }
+    internal class Program
+    {
+        public List<Karta> UstawReke(List<Karta> list)
+        {
+            string kolor = "";
+            int wartosc = 0;
+            Random r = new Random();
+            for (int i = 0; i < 2; i++)
+            {
+                Karta karta = new Karta();
+                kolor = r.Next(1, 5).ToString();
+                if (kolor == "1")
+                {
+                    
+                }
+
+                if (kolor == "2")
+                {
+                    
+                }
+
+                if (kolor == "3")
+                {
+                    
+                }
+                if(kolor == "4")
+            }
+            
+            return list;
+        }
+        public bool Wynik(List<Karta> list)
+        {
+            Random r = new Random();
+            int suma = 0;
+            bool wygrana = false;
+            int krupier = r.Next(2, 22);
+            foreach (var element in list)
+            {
+                suma += int.Parse(element.Wartosc);
+            }
+            if (suma > 21)
+            {
+                wygrana = false;
+            }
+
+            if (suma == 21)
+            {
+                wygrana = true;
+            }
+
+            if (suma > krupier)
+            {
+                wygrana = true;
+            }
+
+            if (krupier > suma)
+            {
+                wygrana = false;
+            }
+            return wygrana;
+        }
+        public static void Main(string[] args)
+        {
+            
+        }
+    }
+}
---
+++ Black Jack/Properties/AssemblyInfo.cs
@@ -0,0 +1,35 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Black_Jack")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Black_Jack")]
+[assembly: AssemblyCopyright("Copyright ©  2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("099C22D1-977D-4A11-BEA1-DC57D742F401")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
